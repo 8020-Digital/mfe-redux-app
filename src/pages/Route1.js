b397e468ecaa7f1a1c81f91876a6036adf2df9b0
PEDRO PAULO - IMAC <pedro.paulods@hotmail.com>
2019-09-02 11:45:33 -0300

    changes to deploy

--- src/pages/Route1.js
+++ src/pages/Route1.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
 
 export default () => {
-    return <h3>Route 1</h3>
+    return (
+        <div>
+            <Helmet>
+                <title>My Title</title>
+                <meta name="description" content="Helmet application" />
+            </Helmet>
+            <h3>Route 1</h3>
+        </div>
+    )
+
 }
